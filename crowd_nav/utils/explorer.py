d53996b04a2972faf2b04e136c1f2e9474a33635
Yuxiang Gao <yuxiang.gao@jhu.edu>
2021-08-04 15:11:54 -0400

    Add progress bar

--- crowd_nav/utils/explorer.py
+++ crowd_nav/utils/explorer.py
@@ -1,6 +1,7 @@
 import logging
 import copy
 import torch
+from tqdm import tqdm
 from crowd_sim.envs.utils.info import *
 
 
@@ -18,8 +19,15 @@ class Explorer(object):
         self.target_model = copy.deepcopy(target_model)
 
     # @profile
-    def run_k_episodes(self, k, phase, update_memory=False, imitation_learning=False, episode=None,
-                       print_failure=False):
+    def run_k_episodes(
+        self,
+        k,
+        phase,
+        update_memory=False,
+        imitation_learning=False,
+        episode=None,
+        print_failure=False,
+    ):
         self.robot.policy.set_phase(phase)
         success_times = []
         collision_times = []
@@ -32,6 +40,12 @@ class Explorer(object):
         cumulative_rewards = []
         collision_cases = []
         timeout_cases = []
+
+        if k != 1:
+            pbar = tqdm(total=k)
+        else:
+            pbar = None
+
         for i in range(k):
             ob = self.env.reset(phase)
             done = False
@@ -61,37 +75,58 @@ class Explorer(object):
                 timeout_cases.append(i)
                 timeout_times.append(self.env.time_limit)
             else:
-                raise ValueError('Invalid end signal from environment')
+                raise ValueError("Invalid end signal from environment")
 
             if update_memory:
                 if isinstance(info, ReachGoal) or isinstance(info, Collision):
                     # only add positive(success) or negative(collision) experience in experience set
                     self.update_memory(states, actions, rewards, imitation_learning)
 
-            cumulative_rewards.append(sum([pow(self.gamma, t * self.robot.time_step * self.robot.v_pref)
-                                           * reward for t, reward in enumerate(rewards)]))
+            cumulative_rewards.append(
+                sum(
+                    [
+                        pow(self.gamma, t * self.robot.time_step * self.robot.v_pref) * reward
+                        for t, reward in enumerate(rewards)
+                    ]
+                )
+            )
+
+            if pbar:
+                pbar.update(1)
 
         success_rate = success / k
         collision_rate = collision / k
         assert success + collision + timeout == k
-        avg_nav_time = sum(success_times) / len(success_times) if success_times else self.env.time_limit
+        avg_nav_time = (
+            sum(success_times) / len(success_times) if success_times else self.env.time_limit
+        )
 
-        extra_info = '' if episode is None else 'in episode {} '.format(episode)
-        logging.info('{:<5} {}has success rate: {:.2f}, collision rate: {:.2f}, nav time: {:.2f}, total reward: {:.4f}'.
-                     format(phase.upper(), extra_info, success_rate, collision_rate, avg_nav_time,
-                            average(cumulative_rewards)))
-        if phase in ['val', 'test']:
+        extra_info = "" if episode is None else "in episode {} ".format(episode)
+        logging.info(
+            "{:<5} {}has success rate: {:.2f}, collision rate: {:.2f}, nav time: {:.2f}, total reward: {:.4f}".format(
+                phase.upper(),
+                extra_info,
+                success_rate,
+                collision_rate,
+                avg_nav_time,
+                average(cumulative_rewards),
+            )
+        )
+        if phase in ["val", "test"]:
             num_step = sum(success_times + collision_times + timeout_times) / self.robot.time_step
-            logging.info('Frequency of being in danger: %.2f and average min separate distance in danger: %.2f',
-                         too_close / num_step, average(min_dist))
+            logging.info(
+                "Frequency of being in danger: %.2f and average min separate distance in danger: %.2f",
+                too_close / num_step,
+                average(min_dist),
+            )
 
         if print_failure:
-            logging.info('Collision cases: ' + ' '.join([str(x) for x in collision_cases]))
-            logging.info('Timeout cases: ' + ' '.join([str(x) for x in timeout_cases]))
+            logging.info("Collision cases: " + " ".join([str(x) for x in collision_cases]))
+            logging.info("Timeout cases: " + " ".join([str(x) for x in timeout_cases]))
 
     def update_memory(self, states, actions, rewards, imitation_learning=False):
         if self.memory is None or self.gamma is None:
-            raise ValueError('Memory or gamma value is not set!')
+            raise ValueError("Memory or gamma value is not set!")
 
         for i, state in enumerate(states):
             reward = rewards[i]
@@ -101,8 +136,14 @@ class Explorer(object):
                 # define the value of states in IL as cumulative discounted rewards, which is the same in RL
                 state = self.target_policy.transform(state)
                 # value = pow(self.gamma, (len(states) - 1 - i) * self.robot.time_step * self.robot.v_pref)
-                value = sum([pow(self.gamma, max(t - i, 0) * self.robot.time_step * self.robot.v_pref) * reward
-                             * (1 if t >= i else 0) for t, reward in enumerate(rewards)])
+                value = sum(
+                    [
+                        pow(self.gamma, max(t - i, 0) * self.robot.time_step * self.robot.v_pref)
+                        * reward
+                        * (1 if t >= i else 0)
+                        for t, reward in enumerate(rewards)
+                    ]
+                )
             else:
                 if i == len(states) - 1:
                     # terminal state
@@ -110,7 +151,9 @@ class Explorer(object):
                 else:
                     next_state = states[i + 1]
                     gamma_bar = pow(self.gamma, self.robot.time_step * self.robot.v_pref)
-                    value = reward + gamma_bar * self.target_model(next_state.unsqueeze(0)).data.item()
+                    value = (
+                        reward + gamma_bar * self.target_model(next_state.unsqueeze(0)).data.item()
+                    )
             value = torch.Tensor([value]).to(self.device)
 
             # # transform state of different human_num into fixed-size tensor
